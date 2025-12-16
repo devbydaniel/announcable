c01a10fc48d22b83f0b9dc76683665c3e47f5c9e
Daniel Benner <daniel@devbydaniel.com>
2025-12-16 15:16:49 +0100

    fix: fix urls again

--- backend/internal/handler/api/shared/objstore.go
+++ backend/internal/handler/api/shared/objstore.go
@@ -5,14 +5,18 @@ import (
 	"net/http/httputil"
 	"net/url"
 
+	"github.com/devbydaniel/announcable/config"
 	"github.com/go-chi/chi/v5"
 )
 
 // HandleObjStore proxies requests to object storage
 func (h *Handlers) HandleObjStore(w http.ResponseWriter, r *http.Request) {
 	h.Log.Trace().Msg("HandleObjStore")
-	// Internal service URL (accessible within Docker network)
-	target, err := url.Parse("http://objstorage:9000")
+
+	// Build target URL from config
+	// Always use HTTP for internal Docker communication (SSL is handled by nginx externally)
+	cfg := config.New()
+	target, err := url.Parse("http://" + cfg.ObjStorage.Endpoint)
 	if err != nil {
 		panic(err)
 	}
